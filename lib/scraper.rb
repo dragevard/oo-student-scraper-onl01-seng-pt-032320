61c7c54a64f809c236438f23931b0542b68421e8
Derek phung <dragevards@gmail.com>
2020-05-17 01:10:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     students = []
     doc = Nokogiri::HTML(open(index_url))
-    doc.css(".student-card")
+    doc.css("div.roster-cards-container")
   end
 
   def self.scrape_profile_page(profile_url)
