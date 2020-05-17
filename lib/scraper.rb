cf4ef5b01070893637077355fcf7badd81327115
Derek phung <dragevards@gmail.com>
2020-05-17 01:35:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-html = open(index_url)
+    html = open(index_url)
     doc = Nokogiri::HTML(html)
     student_cards = doc.css(".student-card a")
     student_cards.collect do |element|
